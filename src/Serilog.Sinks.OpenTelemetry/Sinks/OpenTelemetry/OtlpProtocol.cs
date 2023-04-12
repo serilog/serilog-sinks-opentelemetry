ffaea0aa7417dd5d7decb89968e1f749415c9b9c
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-04-13 09:50:37 +1000

    Introduce an options type; try to align naming etc. more closely with the other core Serilog sinks

---
+++ src/Serilog.Sinks.OpenTelemetry/Sinks/OpenTelemetry/OtlpProtocol.cs
@@ -0,0 +1,17 @@
+﻿namespace Serilog.Sinks.OpenTelemetry;
+
+/// <summary>
+/// Defines the OTLP protocol to use when sending OpenTelemetry data.
+/// </summary>
+public enum OtlpProtocol
+{
+    /// <summary>
+    /// Sends OpenTelemetry data encoded as a protobuf message over gRPC.
+    /// </summary>
+    GrpcProtobuf,
+
+    /// <summary>
+    /// Posts OpenTelemetry data encoded as a protobuf message over HTTP.
+    /// </summary>
+    HttpProtobuf
+}
