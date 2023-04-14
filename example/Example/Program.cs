1d76c9311458097d4499130f90b5bd288fdcf361
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-04-14 15:12:38 +1000

    Include small test harness project

---
+++ example/Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Debugging;
+using Serilog.Sinks.OpenTelemetry;
+
+SelfLog.Enable(Console.Out);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.OpenTelemetry(options =>
+    {
+        options.Endpoint = "http://localhost:4318";
+        options.Protocol = OtlpProtocol.HttpProtobuf;
+    })
+    .CreateLogger();
+
+try
+{
+    Log.Information("Hello, {Name}!", "OpenTelemetry");
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
