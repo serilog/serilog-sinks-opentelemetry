7335864d7e892f351c7c7a1caf53ae0ad5e8a7df
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-05-12 12:32:28 +1000

    Break dependency on OpenTelemetry.SemanticConventions; fixes #78

---
+++ src/Serilog.Sinks.OpenTelemetry/Sinks/OpenTelemetry/SemanticConventions.cs
@@ -0,0 +1,33 @@
+﻿namespace Serilog.Sinks.OpenTelemetry;
+
+/// <summary>
+/// Well-known attribute names.
+/// </summary>
+/// <remarks>See also https://opentelemetry.io/docs/reference/specification/logs/semantic_conventions/.</remarks>
+static class SemanticConventions
+{
+    /// <summary>
+    /// OpenTelemetry standard exception type attribute.
+    /// </summary>
+    public const string AttributeExceptionType = "exception.type";
+
+    /// <summary>
+    /// OpenTelemetry standard exception type attribute.
+    /// </summary>
+    public const string AttributeExceptionMessage = "exception.message";
+    
+    /// <summary>
+    /// OpenTelemetry standard exception type attribute.
+    /// </summary>
+    public const string AttributeExceptionStacktrace = "exception.stacktrace";
+
+    /// <summary>
+    /// A https://messagetemplates.org template, as text. For example, the string <c>Hello {Name}!</c>.
+    /// </summary>
+    public  const string AttributeMessageTemplateText = "message_template.text";
+
+    /// <summary>
+    /// A https://messagetemplates.org template, hashed using MD5 and encoded as a 128-bit hexadecimal value.
+    /// </summary>
+    public const string AttributeMessageTemplateMD5Hash = "message_template.hash.md5";
+}
