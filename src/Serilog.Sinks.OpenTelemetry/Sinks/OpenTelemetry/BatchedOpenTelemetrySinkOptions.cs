42f5ac374c69613a6d95ccbafb5f4fd3db360cb6
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-05-08 11:57:18 +1000

    Updated namespace for BatchingOptions

--- src/Serilog.Sinks.OpenTelemetry/Sinks/OpenTelemetry/BatchedOpenTelemetrySinkOptions.cs
+++ src/Serilog.Sinks.OpenTelemetry/Sinks/OpenTelemetry/BatchedOpenTelemetrySinkOptions.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Serilog.Core.Sinks.Batching;
+using Serilog.Configuration;
 
 namespace Serilog.Sinks.OpenTelemetry;
 
