d28cb90ec4647283112cb711bd3c60c9e5855990
Alberto Monteiro <jose_alberto_monteiro_albuquerque@carrefour.com>
2024-06-20 18:55:27 -0300

    PR review

---
+++ src/Serilog.Sinks.OpenTelemetry/Helpers/OpenTelemetryEnvironmentVariables.cs
@@ -0,0 +1,41 @@
+﻿namespace Serilog.Helpers;
+
+internal static class OpenTelemetryEnvironmentVariables
+{
+    public static string Endpoint
+        => Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT") ?? throw new InvalidOperationException("OTEL_EXPORTER_OTLP_ENDPOINT was not found");
+
+    public static Dictionary<string, string> Headers
+        => GetHeaders(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS"));
+
+    public static Dictionary<string, object> ResourceAttributes
+        => GetResourceAttributes(Environment.GetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES"));
+
+    private static Dictionary<string, string> GetHeaders(string? config)
+    {
+        Dictionary<string, string> configs = [];
+        foreach (var part in config?.Split(',') ?? [])
+        {
+            if (part.Split('=') is { Length: 2 } parts)
+                configs.Add(parts[0], parts[1]);
+            else
+                throw new InvalidOperationException($"Invalid header format: {part}");
+        }
+
+        return configs;
+    }
+
+    private static Dictionary<string, object> GetResourceAttributes(string? config)
+    {
+        Dictionary<string, object> configs = [];
+        foreach (var part in config?.Split(',') ?? [])
+        {
+            if (part.Split('=') is { Length: 2 } parts)
+                configs.Add(parts[0], parts[1]);
+            else
+                throw new InvalidOperationException($"Invalid resourceAttributes format: {part}");
+        }
+
+        return configs;
+    }
+}
---
+++ test/Serilog.Sinks.OpenTelemetry.Tests/OpenTelemetryEnvironmentVariablesTests.cs
@@ -0,0 +1,74 @@
+﻿using Serilog.Helpers;
+using Xunit;
+
+namespace Serilog.Sinks.OpenTelemetry.Tests;
+
+public class OpenTelemetryEnvironmentVariablesTests
+{
+    [Fact]
+    public void EndpointIsObtainedIfWhenPresent()
+    {
+        var endpoint = "http://localhost";
+        Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", endpoint);
+
+        var actual = OpenTelemetryEnvironmentVariables.Endpoint;
+
+        Assert.Equal(endpoint, actual);
+    }
+
+    [Fact]
+    public void EndpointThrowsExceptionIfWhenNotPresent()
+    {
+        var exception = Assert.Throws<InvalidOperationException>(() => OpenTelemetryEnvironmentVariables.Endpoint);
+
+        Assert.Equal("OTEL_EXPORTER_OTLP_ENDPOINT was not found", exception.Message);
+    }
+
+    [Fact]
+    public void HeadersIsObtainedIfWhenPresent()
+    {
+        var headers = "header1=1,header2=2";
+        Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS", headers);
+
+        var actual = OpenTelemetryEnvironmentVariables.Headers;
+
+        Assert.Collection(actual,
+            e => Assert.Equal(("header1", "1"), (e.Key, e.Value)),
+            e => Assert.Equal(("header2", "2"), (e.Key, e.Value)));
+    }
+
+    [Fact]
+    public void HeadersThrowsExceptionIfWhenNotPresent()
+    {
+        var headers = "header1";
+        Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS", headers);
+
+        var exception = Assert.Throws<InvalidOperationException>(() => OpenTelemetryEnvironmentVariables.Headers);
+
+        Assert.Equal("Invalid header format: header1", exception.Message);
+    }
+
+    [Fact]
+    public void ResourceAttributesIsObtainedIfWhenPresent()
+    {
+        var headers = "header1=1,header2=2";
+        Environment.SetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES", headers);
+
+        var actual = OpenTelemetryEnvironmentVariables.ResourceAttributes;
+
+        Assert.Collection(actual,
+            e => Assert.Equal(("header1", "1"), (e.Key, e.Value)),
+            e => Assert.Equal(("header2", "2"), (e.Key, e.Value)));
+    }
+
+    [Fact]
+    public void ResourceAttributesThrowsExceptionIfWhenNotPresent()
+    {
+        var headers = "header1";
+        Environment.SetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES", headers);
+
+        var exception = Assert.Throws<InvalidOperationException>(() => OpenTelemetryEnvironmentVariables.ResourceAttributes);
+
+        Assert.Equal("Invalid resourceAttributes format: header1", exception.Message);
+    }
+}
