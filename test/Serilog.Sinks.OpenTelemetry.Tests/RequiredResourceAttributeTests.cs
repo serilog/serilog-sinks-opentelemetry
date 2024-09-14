08b0e25de67f10f277b4d0fe6db1afaa052248a0
alsi-lawr <alexlawrence1418@gmail.com>
2024-09-14 16:31:36 +0100

    Removed rogue if NETFRAMEWORK

--- test/Serilog.Sinks.OpenTelemetry.Tests/RequiredResourceAttributeTests.cs
+++ test/Serilog.Sinks.OpenTelemetry.Tests/RequiredResourceAttributeTests.cs
@@ -2,8 +2,6 @@
 using Serilog.Sinks.OpenTelemetry.ProtocolHelpers;
 using Serilog.Sinks.OpenTelemetry.Tests.Support;
 using Xunit;
-#if NETFRAMEWORK
-#endif
 
 namespace Serilog.Sinks.OpenTelemetry.Tests;
 
@@ -13,13 +11,10 @@ public class RequiredResourceAttributeTests
     public void ServiceNameIsPreservedWhenPresent()
     {
         var supplied = Some.String();
-        var ra = new Dictionary<string, object>
-        {
-            ["service.name"] = supplied
-        };
+        var ra = new Dictionary<string, object> { ["service.name"] = supplied };
 
         var actual = RequiredResourceAttributes.AddDefaults(ra);
-        
+
         Assert.Equal(supplied, actual["service.name"]);
     }
 
@@ -27,7 +22,7 @@ public class RequiredResourceAttributeTests
     public void MissingServiceNameDefaultsToExecutableName()
     {
         var actual = RequiredResourceAttributes.AddDefaults(new Dictionary<string, object>());
-        
+
         Assert.StartsWith("unknown_service:", (string)actual["service.name"]);
     }
 
@@ -38,6 +33,13 @@ public class RequiredResourceAttributeTests
         Assert.Equal("serilog", actual["telemetry.sdk.name"]);
         Assert.Equal("dotnet", actual["telemetry.sdk.language"]);
         // First character of the version is always expected to be numeric.
-        Assert.True(int.TryParse(((string)actual["telemetry.sdk.version"])[0].ToString(), NumberStyles.Integer, CultureInfo.InvariantCulture, out _));
+        Assert.True(
+            int.TryParse(
+                ((string)actual["telemetry.sdk.version"])[0].ToString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out _
+            )
+        );
     }
 }
